96368787d076cdc718edc010ceb10917cdbac07c
Luan <luan@fatec.sp.gov.br>
2025-09-06 01:08:49 +0000

    crud contato

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
-import { Stack, usePathname, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { ActivityIndicator, View } from "react-native";
-import { loadAuthToken } from "../lib/api";
+import { Stack, usePathname, useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { loadAuthToken } from '../lib/api';
 
 export default function RootLayout() {
   const [loading, setLoading] = useState(true);
@@ -11,15 +11,15 @@ export default function RootLayout() {
   useEffect(() => {
     (async () => {
       const token = await loadAuthToken();
-      const isAuthPage = pathname === "/" || pathname === "/cadastro";
-      if (!token && !isAuthPage) router.replace("/");
+      const isAuthPage = pathname === '/' || pathname === '/cadastro';
+      if (!token && !isAuthPage) router.replace('/');
       setLoading(false);
     })();
   }, [pathname]);
 
   if (loading) {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <ActivityIndicator size="large" />
       </View>
     );
